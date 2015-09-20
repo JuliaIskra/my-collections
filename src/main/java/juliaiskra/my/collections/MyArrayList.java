650e89b691bc5badaeb9b1fa804d096df25a861b
Yuliya Kupryakova <juliaiskra@gmail.com>
2015-09-20 20:47:06 +0300

    Started implementing the Dequeue

--- src/main/java/juliaiskra/my/collections/MyArrayList.java
+++ src/main/java/juliaiskra/my/collections/MyArrayList.java
@@ -10,49 +10,45 @@ import java.util.Arrays;
 public class MyArrayList<E> extends AbstractList<E> {
     private int capacity = 8;
     private E[] array;
-    private int size;
+    private int size = 0;
+    private int offset = 0;
 
     public MyArrayList(Class<E> aClass) {
         //noinspection unchecked
         array = (E[]) Array.newInstance(aClass, capacity);
-        this.size = 0;
     }
 
     @Override
     public E get(int index) {
         checkIndexBoundsForExistingElements(index);
-        return array[index];
+        return array[calculateActualIndex(index)];
+    }
+
+    @Override
+    public E set(int index, E element) {
+        checkIndexBoundsForExistingElements(index);
+        return replaceActualElement(calculateActualIndex(index), element);
     }
 
     @Override
     public void add(int index, E element) {
         checkIndexBoundsForNewElements(index);
 
-        if (size == capacity) {
-            capacity = capacity * 2;
-            resizeArray();
-        }
-
-        size++;
-        for (int i = index; i < size; i++) {
-            element = replaceElement(i, element);
-        }
-    }
-
-    @Override
-    public E set(int index, E element) {
-        checkIndexBoundsForExistingElements(index);
-        return replaceElement(index, element);
+        increaseSize();
+        int actualIndex = calculateActualIndex(index);
+        shiftActualArrayFrom(actualIndex);
+        replaceActualElement(actualIndex, element);
     }
 
     @Override
     public E remove(int index) {
         checkIndexBoundsForExistingElements(index);
-        E old = null;
-        for (int i = size - 1; i >= index; i--) {
-            old = replaceElement(i, old);
-        }
-        size--;
+
+        int actualIndex = calculateActualIndex(index);
+        E old = replaceActualElement(actualIndex, null);
+        shiftActualArrayTo(actualIndex);
+        decreaseSize();
+
         return old;
     }
 
@@ -83,25 +79,94 @@ public class MyArrayList<E> extends AbstractList<E> {
         }
     }
 
-    private void resizeArray() {
-        array = Arrays.copyOf(array, capacity);
-    }
-
-    private E replaceElement(int index, E element) {
-        E old = array[index];
-        array[index] = element;
-        return old;
-    }
-
     private void checkIndexBoundsForExistingElements(int index) {
-        if (index < 0 || index > size - 1) {
+        if (index >= 0 && index < size) {
+            return;
+        } else {
             throw new IndexOutOfBoundsException();
         }
     }
 
     private void checkIndexBoundsForNewElements(int index) {
-        if (index < 0 || index > size) {
+        if (index >= 0 && index <= size) {
+            return;
+        } else {
             throw new IndexOutOfBoundsException();
         }
+    }
+
+    private void increaseSize() {
+        if (size == capacity) {
+            capacity = capacity * 2;
+            resizeArray();
+        }
+        size++;
+    }
+
+    private void decreaseSize() {
+        size--;
+    }
+
+    private void resizeArray() {
+        // todo add newCapacity as a parameter
+        // todo change implementation
+        array = Arrays.copyOf(array, capacity);
+    }
+
+    private void shiftActualArrayTo(int actualIndex) {
+        // todo implement
+        if (closerToLeft(actualIndex)) {
+            shiftFromLeftTo(actualIndex);
+        } else {
+            shiftFromRightTo(actualIndex);
+        }
+    }
+
+    private boolean closerToLeft(int actualIndex) {
+        //todo implement
+        return false;
+    }
+
+    private void shiftFromLeftTo(int actualIndex) {
+        // todo implement
+
+    }
+
+    private void shiftFromRightTo(int actualIndex) {
+        // todo implement
+
+    }
+
+    private void shiftActualArrayFrom(int actualIndex) {
+        // todo implement
+        if (closerToLeft(actualIndex)) {
+            shiftToLeftFrom(actualIndex);
+        } else {
+            shiftToRightFrom(actualIndex);
+        }
+    }
+
+    private void shiftToLeftFrom(int actualIndex) {
+        //todo implement
+
+    }
+
+    private void shiftToRightFrom(int actualIndex) {
+        //todo implement
+
+    }
+
+    private E replaceActualElement(int actualIndex, E element) {
+        E old = array[actualIndex];
+        array[actualIndex] = element;
+        return old;
+    }
+
+    private int calculateActualIndex(int index) {
+        int actualIndex = index + offset;
+        if (actualIndex >= capacity) {
+            actualIndex = actualIndex - capacity;
+        }
+        return actualIndex;
     }
 }
